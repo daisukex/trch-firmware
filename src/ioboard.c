91405d6824601712a75d856b6ca4c564d74ff5fe
Daisuke Sasaki <daisuke@spacecubics.com>
2024-02-24 03:34:13 +0900

    ioboard: Add initialization code for IO Board of SC-SAT1 Main OBC
    The initial values of UART_DE and UART_REB are set to be
    inactive. Software must set these signals to the active state when
    using the TRCH UART interface.
    
    VD3V3_SYS_EN must always be set to High level.
    
    Signed-off-by: Daisuke Sasaki <daisuke@spacecubics.com>

--- src/ioboard.c
+++ src/ioboard.c
@@ -6,7 +6,13 @@
  * function.  Note that XC8 for PIC16LF877 with C90 library doesn't
  * have weak attribute.  This will be linked at very last to full
  * fill if not defined. */
+#include "ioboard.h"
+
 int ioboard_init(void)
 {
+        UART_DE = 0;
+        UART_RE_B = 1;
+        VD3V3_SYS_EN = 1;
+
         return 0;
 }
